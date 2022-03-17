1d54b90ab3fda30e197baed1f00884e4dfc2fb85
Mike Worden <michael.worden@icloud.com>
2022-03-17 11:31:08 -0600

    Added code to evaluate biometrics

--- LoginScreen/ViewController.swift
+++ LoginScreen/ViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import LocalAuthentication
+
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var loginButton: UIButton!
+	@IBOutlet weak var blockImage: UIImageView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+		blockImage.transform = CGAffineTransform(rotationAngle: 3.1414159265)
+		
+		loginButton.layer.backgroundColor = UIColor(red: 207/255, green: 80/255, blue: 84/255, alpha: 1).cgColor
+		loginButton.layer.cornerRadius = 22
 	}
-
-
+	
+	@IBAction func loginButtonPressed(_ sender: Any) {
+		let localAuthenticationContext = LAContext()
+		var authorizationError: NSError?
+		
+		let authReason = "Authentication required to Login"
+		
+		if localAuthenticationContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &authorizationError) {
+			localAuthenticationContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: authReason) {
+				success, evaluateError in
+				if success {
+					print("Successfully logged in via TouchID")
+				} else {
+					guard let error = evaluateError else {
+						return
+					}
+					print (error)
+				}
+			}
+			
+		} else {
+			guard let error = authorizationError else {
+				return
+			}
+			print (error)
+		}
+	}
+	
+	
 }
 
